a056827c35e729c10ee8ce33bcc1a9c2c67e3c1a
Kamyar <caracicatriz.kt@gmail.com>
2023-10-26 10:55:40 -0600

    added authentication

--- server/models.py
+++ server/models.py
@@ -1,16 +1,34 @@
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
 
-from config import db
+from config import db, bcrypt
 
 # Models go here!
 
 class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String)
-    password = db.Column(db.String)
+    username = db.Column(db.String, nullable = False, unique = True)
+    email = db.Column(db.String)
+    _password_hash = db.Column(db.String, nullable=False)
+
     predictions = db.relationship('Prediction', back_populates='user')
+
+    #password stuff
+    @hybrid_property
+    def password_hash(self):
+        return self._password_hash
+    @password_hash.setter
+    def password_hash(self, password):
+        password_hash = bcrypt.generate_password_hash(
+            password.encode('utf-8')
+        )
+        self._password_hash = password_hash.decode('utf-8')
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(
+            self._password_hash,
+            password.encode('utf-8')
+        )
 
     serialize_rules = ('-predictions.user',)  # Exclude user relationship from serialization
 
