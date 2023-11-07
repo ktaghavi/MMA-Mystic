099b6a3b045f4857a3a8b9e164e9b6c5a906c25c
Kamyar <caracicatriz.kt@gmail.com>
2023-11-07 03:16:59 -0700

    Successfully seeded fightersgit add .!

---
+++ server/import_fighters.py
@@ -0,0 +1,48 @@
+import pandas as pd
+from app import app
+from models import db, Fighter
+from datetime import datetime
+import math
+
+def import_fighter_data():
+    # Load fighter data from CSV
+    fighter_data = pd.read_csv('data/raw_fighter_details.csv')
+
+    with app.app_context():
+        db.create_all()
+
+        # Create Fighter objects from the CSV data
+        fighters = []
+        for _, row in fighter_data.iterrows():
+            dob_str = str(row['DOB'])  # Convert to string
+            dob = None  # Default value for DOB
+
+            # Check for 'nan' values using math.isnan
+            if not isinstance(row['DOB'], float) or not math.isnan(row['DOB']):
+                # Attempt to convert the date string to a date object
+                dob = datetime.strptime(dob_str, '%d-%b-%y').date()
+            
+            fighter = Fighter(
+                name=row['name'],
+                height=row['height'],
+                weight=row['weight'],
+                reach=row['reach'],
+                stance=row['stance'],
+                DOB=dob,  # Use the date object or None
+                SLpM=row['SLpM'],
+                Str_Acc=row['Str_Acc'],
+                SApM=row['SApM'],
+                Str_Def=row['Str_Def'],
+                TD_Avg=row['TD_Avg'],
+                TD_Acc=row['TD_Acc'],
+                TD_Def=row['TD_Def'],
+                Sub_Avg=row['Sub_Avg']
+            )
+            fighters.append(fighter)
+
+        db.session.add_all(fighters)
+        db.session.commit()
+
+if __name__ == '__main__':
+    import_fighter_data()
+    print("Data import completed.")
--- server/seed.py
+++ server/seed.py
@@ -22,25 +22,25 @@ if __name__ == '__main__':
         for _ in range(6):
             username = fake.user_name()
             email = fake.email()
-            password_hash = '123'
+            password_hash = '123test!@#'
             user = User(username=username, email=email, password_hash=password_hash)
             users.append(user)
         
         db.session.add_all(users)
         db.session.commit()
         
-        # Create real fighters
-        fighters = [
-            Fighter(name='Islam Makhachev', weightclass='Lightweight', height=70, stance='Southpaw', dob=date.fromisoformat('1991-10-27'), SigLpM=2.46, StrAcc=0.6, SApM=1.27, StrDef=0.61, TDAvg=3.17, TDAcc=0.6, TDDef=0.9, SubAvg=1.0),
-            Fighter(name='Alexander Volkanovski', nickname='THE GREAT', weightclass='Featherweight', height=66, stance='Orthodox', dob=date.fromisoformat('1988-09-29'), SigLpM=6.19, StrAcc=0.57, SApM=3.42, StrDef=0.58, TDAvg=1.84, TDAcc=0.37, TDDef=0.7, SubAvg=0.2),
-            Fighter(name='Khamzat Chimaev', weightclass='Middleweight', height=74, stance='Orthodox', dob=date.fromisoformat('1994-05-01'), SigLpM=5.72, StrAcc=0.58, SApM=3.46, StrDef=0.42, TDAvg=3.99, TDAcc=0.46, TDDef=1.0, SubAvg=2.7),
-            Fighter(name='Kamaru Usman', nickname='THE NIGERIAN NIGHTMARE', weightclass='Welterweight', height=72, stance='Switch', dob=date.fromisoformat('1987-05-11'), SigLpM=4.36, StrAcc=0.2, SApM=2.74, StrDef=0.54, TDAvg=2.82, TDAcc=0.45, TDDef=0.89, SubAvg=0.1),
-            Fighter(name='Magomed Ankalaev', weightclass='Light Heavyweight', height=75, stance='Orthodox', dob=date.fromisoformat('1992-06-02'), SigLpM=3.56, StrAcc=0.52, SApM=2.2, StrDef=0.58, TDAvg=1.07, TDAcc=0.31, TDDef=0.86, SubAvg=0.0),
-            Fighter(name='Johnny Walker', weightclass='Light Heavyweight', height=78, stance='Orthodox', dob=date.fromisoformat('1992-03-30'), SigLpM=3.87, StrAcc=0.56, SApM=2.0, StrDef=0.45, TDAvg=0.5, TDAcc=1.0, TDDef=0.57, SubAvg=1.0)
-        ]
+        # # Create real fighters
+        # fighters = [
+        #     Fighter(name='Islam Makhachev', weightclass='Lightweight', height=70, stance='Southpaw', dob=date.fromisoformat('1991-10-27'), SigLpM=2.46, StrAcc=0.6, SApM=1.27, StrDef=0.61, TDAvg=3.17, TDAcc=0.6, TDDef=0.9, SubAvg=1.0),
+        #     Fighter(name='Alexander Volkanovski', nickname='THE GREAT', weightclass='Featherweight', height=66, stance='Orthodox', dob=date.fromisoformat('1988-09-29'), SigLpM=6.19, StrAcc=0.57, SApM=3.42, StrDef=0.58, TDAvg=1.84, TDAcc=0.37, TDDef=0.7, SubAvg=0.2),
+        #     Fighter(name='Khamzat Chimaev', weightclass='Middleweight', height=74, stance='Orthodox', dob=date.fromisoformat('1994-05-01'), SigLpM=5.72, StrAcc=0.58, SApM=3.46, StrDef=0.42, TDAvg=3.99, TDAcc=0.46, TDDef=1.0, SubAvg=2.7),
+        #     Fighter(name='Kamaru Usman', nickname='THE NIGERIAN NIGHTMARE', weightclass='Welterweight', height=72, stance='Switch', dob=date.fromisoformat('1987-05-11'), SigLpM=4.36, StrAcc=0.2, SApM=2.74, StrDef=0.54, TDAvg=2.82, TDAcc=0.45, TDDef=0.89, SubAvg=0.1),
+        #     Fighter(name='Magomed Ankalaev', weightclass='Light Heavyweight', height=75, stance='Orthodox', dob=date.fromisoformat('1992-06-02'), SigLpM=3.56, StrAcc=0.52, SApM=2.2, StrDef=0.58, TDAvg=1.07, TDAcc=0.31, TDDef=0.86, SubAvg=0.0),
+        #     Fighter(name='Johnny Walker', weightclass='Light Heavyweight', height=78, stance='Orthodox', dob=date.fromisoformat('1992-03-30'), SigLpM=3.87, StrAcc=0.56, SApM=2.0, StrDef=0.45, TDAvg=0.5, TDAcc=1.0, TDDef=0.57, SubAvg=1.0)
+        # ]
 
-        db.session.add_all(fighters)
-        db.session.commit()
+        # db.session.add_all(fighters)
+        # db.session.commit()
 
         # Create fight history
         fights = [
